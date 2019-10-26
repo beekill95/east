Fix wrong order when constructing location matrix.

east/groundtruth.py
@@ -89,19 +89,18 @@ def _generate_geometry_map_np(shrinked_text_boxes_img, text_boxes):
         # bboxes[mask] = min_bbox.flatten()
         # FIXME: don't use loop, use numpy broadcast to do the job.
         flatten_min_bbox = min_bbox.flatten()
-        for i in range(len(flatten_min_bbox)):
+        for i in range(8):
             bboxes[i][mask] = flatten_min_bbox[i]
 
         # Asign angles to geometry map.
-        geometry_map[4, :, :][mask] = _calculate_rotation_angle(min_bbox)
+        geometry_map[4][mask] = _calculate_rotation_angle(min_bbox)
 
     location_mask = shrinked_text_boxes_img > 0
     xv, yv = np.meshgrid(np.arange(0, img_shape[0]),
                          np.arange(0, img_shape[1]))
-    locations = np.stack([xv, yv]) * 4
+    locations = np.stack([yv, xv]) * 4
 
-    geometry_map[:4, :, :] = rbox.generate_rbox_np(
-        locations, bboxes, location_mask)
+    geometry_map[:4] = rbox.generate_rbox_np(locations, bboxes, location_mask)
 
     return geometry_map
 

east/rbox.py
@@ -38,8 +38,8 @@ def generate_rbox_np(locations, bounding_boxes, mask):
     distances = np.zeros((4,) + mask.shape)
 
     # Edges.
-    edges[:6, :, :] = bounding_boxes[:6, :, :] - bounding_boxes[2:, :, :]
-    edges[6:8, :, :] = bounding_boxes[6:8, :, :] - bounding_boxes[:2, :, :]
+    edges[:6] = bounding_boxes[:6] - bounding_boxes[2:]
+    edges[6:8] = bounding_boxes[6:8] - bounding_boxes[:2]
 
     # Calculate the distances.
     for i in range(4):