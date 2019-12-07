Fix calculating rotation of boxes should be based on reordered vertices instead of long edge.

east/groundtruth.py
@@ -1,5 +1,5 @@
 from east import geometry, rbox
-from math import atan
+from math import atan2
 import numpy as np
 from PIL import Image, ImageDraw
 
@@ -10,14 +10,15 @@ def _reorder_vertices(rectangular):
     """
     s = rectangular[:, 0] + rectangular[:, 1]
     min_s_idx = np.argmin(s) - 1
-    return np.asarray([rectangular[(min_s_idx + i) % 4] for i in range(4)])
+    return np.asarray([rectangular[(min_s_idx + i) % 4]
+                       for i in range(4)])
 
 
 def _draw_shrinked_text_boxes(image, text_boxes, offset):
     img_height, img_width, _ = np.shape(image)
 
-    shrinked_text_boxes = [
-        geometry.shrink_polygon(box, offset) for box in text_boxes]
+    shrinked_text_boxes = [geometry.shrink_polygon(box, offset)
+                           for box in text_boxes]
 
     canvas = Image.new("L", (img_width, img_height))
     draw = ImageDraw.Draw(canvas)
@@ -36,25 +37,20 @@ def _generate_score_map(shrinked_text_boxes_img):
 
 def _calculate_rotation_angle(rectangular):
     """
-    Calculate the rotation angle of a rectangular, based on the longer edge.
-    |rectangular| is a 4x2 numpy array.
+    Calculate the rotation angle of a rectangular, based on the top edge.
+    |rectangular| is a 4x2 numpy array. Its vertices should be in clock-wise order,
+    starting from bottom-left vertex.
     """
-    first_edge = rectangular[1] - rectangular[0]
-    second_edge = rectangular[-1] - rectangular[0]
-
-    if geometry.magnitude(first_edge) > geometry.magnitude(second_edge):
-        return atan(first_edge[1] / first_edge[0])
-    else:
-        return atan(second_edge[1] / second_edge[0])
+    edge = rectangular[2] - rectangular[1]
+    return atan2(edge[1], edge[0])
 
 
 def _generate_geometry_map(shrinked_text_boxes_img, text_boxes):
     # FIXME: handle the case when boxes are triangular, in that case,
     # minimum bounding boxes might be wrong.
     min_bboxes = (geometry.minimum_bounding_box(box) for box in text_boxes)
-    min_bboxes = ((_reorder_vertices(box), _calculate_rotation_angle(box))
-                  for box, _ in min_bboxes)
-    min_bboxes = list(min_bboxes)
+    min_bboxes = list(_reorder_vertices(box) for box, _ in min_bboxes)
+    angles = list(_calculate_rotation_angle(box) for box in min_bboxes)
 
     img_shape = shrinked_text_boxes_img.shape
     geometry_map = np.zeros((5,) + img_shape)
@@ -64,12 +60,13 @@ def _generate_geometry_map(shrinked_text_boxes_img, text_boxes):
         r, c = non_zero_rows[i], non_zero_cols[i]
 
         color = shrinked_text_boxes_img[r, c]
-        bbox, angle = min_bboxes[color - 1]
+        bbox = min_bboxes[color - 1]
 
         # AABB & angle.
-        geometry_map[:4, r, c] = rbox.generate_rbox(
-            (c * 4, r * 4), bbox, (img_shape[0] * 4, img_shape[1] * 4))
-        geometry_map[4, r, c] = angle
+        geometry_map[:4, r, c] = rbox.generate_rbox((c * 4, r * 4),
+                                                    bbox,
+                                                    (img_shape[0] * 4, img_shape[1] * 4))
+        geometry_map[4, r, c] = angles[color - 1]
 
     return geometry_map
 
@@ -102,8 +99,10 @@ def _generate_geometry_map_np(shrinked_text_boxes_img, text_boxes):
                          np.arange(0, img_shape[1]))
     locations = np.stack([xv, yv]) * 4
 
-    geometry_map[:4] = rbox.generate_rbox_np(
-        locations, bboxes, location_mask, (img_shape[0] * 4, img_shape[1] * 4))
+    geometry_map[:4] = rbox.generate_rbox_np(locations,
+                                             bboxes,
+                                             location_mask,
+                                             (img_shape[0] * 4, img_shape[1] * 4))
 
     return geometry_map
 
@@ -111,12 +110,13 @@ def _generate_geometry_map_np(shrinked_text_boxes_img, text_boxes):
 def generate_ground_truth(image, text_boxes, score_map_offset=10):
     # FIXME: the offset should be changed accordingly to the size of the box,
     # or else the offsetted polygon would fall outside of the box.
-    shrinked_text_boxes_img = _draw_shrinked_text_boxes(
-        image, text_boxes, score_map_offset)
+    shrinked_text_boxes_img = _draw_shrinked_text_boxes(image,
+                                                        text_boxes,
+                                                        score_map_offset)
 
     # FIXME: we should filtered out boxes that are too small.
     score_map = _generate_score_map(shrinked_text_boxes_img)
-    geometry_map = _generate_geometry_map_np(
-        shrinked_text_boxes_img, text_boxes)
+    geometry_map = _generate_geometry_map_np(shrinked_text_boxes_img,
+                                             text_boxes)
     gt_map = np.concatenate((score_map, geometry_map), axis=0)
     return np.moveaxis(gt_map, 0, -1)